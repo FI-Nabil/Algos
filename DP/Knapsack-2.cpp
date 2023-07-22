0c4b9f004c91021ed1a35a6406c54c7ffdd648b5
FI-Nabil <63797144+FI-Nabil@users.noreply.github.com>
2023-07-22 15:20:54 +0600

    Update Knapsack-2.cpp

--- DP/Knapsack-2.cpp
+++ DP/Knapsack-2.cpp
@@ -1,3 +1,39 @@
+/* Recursive */
+/*
+1. rec(i, val) returns minimum weight, taking any subsets from 1........i
+2. masximum value which retursn (weight <= given_weight) is the answer.
+*/
+
+
+ll n, weight;
+ll v[101], w[101];
+const ll mx = 1e5 +10;
+const ll inf = 1e12;
+ll dp[101][mx];
+ll rec (ll i, ll val){
+    if(val == 0) return 0;
+    if(val < 0 or i == 0) return inf;
+    if(dp[i][val] != -1) return dp[i][val];
+
+    ll wt = 0;
+    wt = w[i] + rec(i-1, val-v[i]);
+    wt = min(wt, rec(i-1, val));
+    return dp[i][val] = wt;
+}
+
+void dracarys(){
+        cin >> n >> weight;
+        for (ll i=1; i<=n; i++){
+            cin >> w[i] >> v[i];
+        }
+        memset(dp, -1, sizeof dp);
+        for (ll i=mx-5; i>=0; i--){
+            if(rec(n, i) <= weight){
+                cout << i <<'\n';
+                return;
+            }
+        }
+}
 
 
 /* Iterative */
